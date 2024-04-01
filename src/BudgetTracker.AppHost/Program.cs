d78ced955322e5d3e20a0654e24489a9c5f08203
Jonas Arvidson <pownas@outlook.com>
2024-04-01 14:03:20 +0200

    Remove aspire-project

--- src/BudgetTracker.AppHost/Program.cs
+++
@@ -1,8 +0,0 @@
-var builder = DistributedApplication.CreateBuilder(args);
-
-var apiService = builder.AddProject<Projects.BudgetTracker_ApiService>("apiservice");
-
-builder.AddProject<Projects.BudgetTracker_Web>("webfrontend")
-    .WithReference(apiService);
-
-builder.Build().Run();
