9b2e231107df90d3eb96881048033bb62f9a5466
Jonas Arvidson <pownas@outlook.com>
2024-04-01 15:22:09 +0200

    Update Transaction.cs
    Chage to Guid

--- src/BudgetTracker.Core/Models/Transaction.cs
+++ src/BudgetTracker.Core/Models/Transaction.cs
@@ -2,7 +2,7 @@
 
 public class Transaction
 {
-    public int Id { get; set; } = 0;
+    public Guid Id { get; set; } = Guid.NewGuid();
     public string Name { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
     public decimal Amount { get; set; } = 0;
