3fa9d4af096a23d133fa1028dfdec1baff0b1fd1
Jonas Arvidson <pownas@outlook.com>
2024-04-01 16:46:06 +0200

    Adding Swagger and Docs

--- src/BudgetTracker.Web/Program.cs
+++ src/BudgetTracker.Web/Program.cs
@@ -4,6 +4,7 @@ using BudgetTracker.Web.Data;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -36,6 +37,30 @@ builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.Requ
 
 builder.Services.AddSingleton<IEmailSender<ApplicationUser>, IdentityNoOpEmailSender>();
 
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { 
+        Title = "BudgetTracker API", 
+        Version = "v1",
+        Description = "A simple BudgetTracker Web API",
+        Contact = new OpenApiContact
+        {
+            Name = "Pownas",
+            Email = "hello@pownas.se",
+            Url = new Uri("https://www.pownas.se"),
+        },
+        License = new OpenApiLicense
+        {
+            Name = "Use under MIT-license",
+            Url = new Uri("https://opensource.org/license/MIT"),
+        },
+        TermsOfService = new Uri("https://example.com/terms"),
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -50,6 +75,9 @@ else
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
